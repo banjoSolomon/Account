508348946260e4dbc64c4c92d445be3e3f75ec6d
banjoSolomon <ayomidebanjo68@gmail.com>
2024-06-26 11:50:57 +0100

    updated

--- account/views.py
+++ account/views.py
@@ -1,21 +1,27 @@
-
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Account
-from .serializers import AccountSerializer
+from .serializers import AccountSerializer, AccountCreateSerializer
 
 
 # Create your views here.
-@api_view()
+@api_view(['GET', 'POST'])
 def list_account(request):
-    accounts = Account.objects.all()
-    serializer = AccountSerializer(accounts, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+    if request.method == 'GET':
+        accounts = Account.objects.all()
+        serializer = AccountSerializer(accounts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = AccountCreateSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 @api_view()
 def account_detail(request, pk):
-    account = Account.objects.get(pk=pk)
+    account = get_object_or_404(Account, pk=pk)
     serializer = AccountSerializer(account)
     return Response(serializer.data, status=status.HTTP_200_OK)
