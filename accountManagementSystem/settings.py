0cec7cf69c78a2a74d48eb70ba484da06933d86d
banjoSolomon <ayomidebanjo68@gmail.com>
2024-06-27 15:46:01 +0100

    updated

--- accountManagementSystem/settings.py
+++ accountManagementSystem/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'accountManagementSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'account_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Solomon11',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
